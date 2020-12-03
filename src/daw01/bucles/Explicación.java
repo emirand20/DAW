27d2f8da1d5751b56461fe8a5a64361809c4ea67
emirand20 <emirand20@jaumebalmes.net>
2020-12-03 12:00:12 +0100

    version 3

---
+++ src/daw01/bucles/Explicación.java
@@ -0,0 +1,28 @@
+package daw01.bucles;
+
+public class Explicación {
+    public static void main(String[] args) {
+        // Un bucle es un sentencia que hace algo mientras se cumpla una condición
+        //
+        //        // 1. while
+        //        /*
+        //        while("aqui pase algo"){
+        //            HAZ ALGO!!
+        //        }*/
+        //      /*  int cont = 0;
+        //        while (cont < 10) {
+        //            System.out.println(cont++);
+        //        }*/
+        //        // 2. for
+        //      /*  for (int i = 512; i >= 500; --i) {
+        //            System.out.println(i);
+        //        }*/
+        //        // mientras i sea mayor o igual  500 pinta --i
+        //        //3. do while
+        //        int i = 0;
+        //        do {
+        //            System.out.println(i);
+        //        } while (i < 10);
+        //        // i++ != ++i && --i != i--
+    }
+}
