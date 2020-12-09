c8b0cdb527b98bdce4352f1d609facf9c7f31324
javim <emirand20@jaumebalmes.net>
2020-12-09 18:02:18 +0100

    clase

--- src/daw01/condicionales_II/Ex03.java
+++ src/daw01/condicionales_II/Ex03.java
@@ -1,7 +1,28 @@
 package daw01.condicionales_II;
 
+import java.util.Scanner;
+
 public class Ex03 {
     public static void main(String[] args) {
+        int aleatorio = (int) (Math.random() * 10);
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("¿A que planta desea ir?");
+        int planta = entrada.nextInt();
+
+        if (planta == aleatorio) {
+            System.out.println("Esta en la " + aleatorio + "planta");
+        } else if (planta > 14) {
+            System.out.println("Planta desconocida");
+        } else if (planta > 7) {
+            System.out.println("Zona restringida!");
+        } else if (planta >= -4 || planta <= 7) {
+        }
+        if (aleatorio > -4) {
+            System.out.println("Esta subiendo");
+
+        } else if (aleatorio < 7) {
+            System.out.println("Esta bajando");
+        }
 
     }
 }
---
+++ src/daw01/condicionales_II/Ex03String.java
@@ -0,0 +1,12 @@
+package daw01.condicionales_II;
+
+import java.util.Scanner;
+
+public class Ex03String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String planta;
+        System.out.println("Esta en la panta 3 a donde desea ir?");
+        
+    }
+}
