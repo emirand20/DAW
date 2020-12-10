f8c22ed7db826bc83bd99b13d67f3638bc3251bd
emirand20 <emirand20@jaumebalmes.net>
2020-12-10 20:42:59 +0100

    ascensor

--- src/daw01/condicionales_II/Ex03String.java
+++ src/daw01/condicionales_II/Ex03String.java
@@ -4,16 +4,53 @@ import java.util.Scanner;
 
 public class Ex03String {
     public static void main(String[] args) {
+        String[] pisos = {"S1", "S2", "S3", "S4", "PB", "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9"};
+
+        int aleatorio = (int) (Math.random() * 12);
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Esta en la panta 3 a donde desea ir?");
-        String p, pb, s;
 
-        switch (p) {
-            case "P8":
-                System.out.println("");
-            case "P9":
+        String ubiPiso = "";
+        do {
+            ubiPiso = pisos[aleatorio];
+        } while (ubiPiso.equals("P8") || ubiPiso.equals("P9"));
 
+        System.out.println("El ascensor esta en la planta: " + ubiPiso);
 
+        System.out.println("A que planta quiere ir?");
+        String plantaNueva = entrada.next();
+
+        boolean busca = false;
+        for (int i = 0; i < pisos.length && !busca; i++) {
+            if (pisos[i].equals(plantaNueva)) {
+                busca = true;
+            }
+
+        }
+
+        if (busca) {
+            if (plantaNueva.equals(ubiPiso)) {
+                System.out.println("Ya esta ahi");
+            } else if (plantaNueva.equals("P8") || plantaNueva.equals("P9")) {
+                System.out.println("Planta restringida");
+            } else {
+                int numPlantaNueva = 0;
+                int numUbi = 0;
+                for (int i = 0; i < pisos.length; i++) {
+                    if (plantaNueva.equals(pisos[i])) {
+                        numPlantaNueva = i;
+                    }
+                    if (ubiPiso.equals(pisos[i])) {
+                        numUbi = i;
+                    }
+                }
+                if (numPlantaNueva < numUbi) {
+                    System.out.println("Ascensor bajando a la planta " + plantaNueva);
+                } else {
+                    System.out.println("Ascensor subiendo a la planta " + plantaNueva);
+                }
+            }
+        } else {
+            System.out.println("Su planta no existe");
         }
 
 
