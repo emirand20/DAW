320e05e6d3c8f0803b2379f5d4f6a9c626c6f48c
emirand20 <emirand20@jaumebalmes.net>
2021-01-16 18:22:29 +0100

    clas

--- src/daw01/exClase/Act05_07.java
+++ src/daw01/exClase/Act05_07.java
@@ -4,20 +4,42 @@ import java.util.Scanner;
 
 public class Act05_07 {
     public static void main(String[] args) {
+//        Leer una cadena con la clase Scanner que será nuestro NIF,  sin espacios en los 8 caracteres de nº i letra, de forma que será 9 caracteres exactamente, si la longitud de la cadena no es de 9 caracteres, indica:
+//        “NIF incorrecto, has introducido X caracteres”, si la longitud de NIF es correcta, entonces el programa comprobará que los 8 primeros caracteres son números,
+//        si no lo son, el mensaje será: NIF incorrecto. Error específicamente los 8 dígitos de la parte numérica”.
+//        También se pedirá que se compruebe si el último carácter es letra mayúscula o minúscula, válida.
+//        Si la letra introducida es incorrecta, el programa dará el mensaje de error: NIF incorrecto. Letra no válida.
 
-        int[] numeros = new int[2];
+//        int[] numeros = new int[8];
+//        String[] letra = new String[0];
+//        Scanner teclado = new Scanner(System.in);
+//        int i;
+//        for (i = 0; i < numeros.length; i++) {
+//            System.out.printf("Introduzca el número " + i + " de su NIF:" + "\n", i + 1);
+//            numeros[i] = teclado.nextInt();
+//        }
+//        for (int j = 0; j < letra.length; j++) {
+//            System.out.println("Introduzca la letra: ");
+//            letra[j] = teclado.next();
+//        }
+//
+//        //Te muestra el array que se a escrito
+//        for (i = 0; i < numeros.length; i++) {
+//            System.out.println(numeros[i]);
+//
+//        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dame tu DNI");
+        String dni = sc.next();
 
-        Scanner teclado = new Scanner(System.in);
-        int i;
-        for (i = 0; i < numeros.length; i++) {
-            System.out.printf("Introduzca el número " + i + " de su NIF:" + "\n", i + 1);
-            numeros[i] = teclado.nextInt();
-        }
-        //Te muestra el array que se a escrito
-        for (i = 0; i < numeros.length; i++) {
-            System.out.println(numeros[i]);
-            
+        //1. ver que son numeros
+
+        String aux = "";
+
+        for (int i = 0; i < dni.length() - 1; i++) {
+            aux += dni.charAt(i);
         }
     }
+
 }
 
--- src/daw01/exClase/Act06_05.java
+++ src/daw01/exClase/Act06_05.java
@@ -4,17 +4,16 @@ import java.util.Random;
 
 public class Act06_05 {
     public static void main(String[] args) {
-        Random rd = new Random();
-        int valores[] = {1, 1, 1, 1, 1, 1, 1, 1, 2};
-        String letas[] = {"X", "X"};
+        int num = 1 + (int) (Math.random() * 10);
 
-        for (int i = 0; i < 8; i++) {
-            int a = (int) (Math.random() * valores.length);
-
-            for (int j = 0; j < 2; j++) {
-                int b = (int) (Math.random() * letas.length);
-
-            }
+        if (num < 7) {
+            System.out.println(1);
+        } else if (num == 8 || num == 9) {
+            System.out.println("X");
+        } else {
+            System.out.println(2);
         }
     }
 }
+
+
