670254b7d10d25c5e4df94263232f0f8f9513c9b
cmiran0 <cmirand0@jaumebalmes.net>
2021-02-11 10:12:46 +0100

    Aquí te creo un objeto y algo chulo para que vayas pensando!

---
+++ src/daw01/cmiranda/App.java
@@ -0,0 +1,11 @@
+package daw01.cmiranda;
+
+import java.util.Scanner;
+import daw01.cmiranda.Dni;
+
+public class App {
+    public static void main(String[] args) {
+        Dni criss = new Dni("24545349W");
+    }
+
+}
