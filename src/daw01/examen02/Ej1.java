51949ecd9e5f383c493d7cfabdb6c7697bd241a6
emirand20 <emirand20@jaumebalmes.net>
2021-05-05 17:50:40 +0200

    ExamUf1

--- src/daw01/examen02/Ej1.java
+++ src/daw01/examen02/Ej1.java
@@ -1,25 +1,26 @@
 package daw01.examen02;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ej1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Que color eliges, entre estos cuatro?: azul, rojo, amarillo o verde");
-        String color = sc.next();
+        String color = sc.next().toLowerCase(Locale.ROOT);
         //int cont = 0;
         switch (color) {
             case "azul":
-                System.out.println("El color " + color.equalsIgnoreCase("Azul") + ", en inglés se escribe blue");
+                System.out.println("El color en inglés se escribe blue");
                 break;
             case "rojo":
-                System.out.println("El color " + color.equalsIgnoreCase("Rojo") + ", en inglés se escribe red");
+                System.out.println("El color en inglés se escribe red");
                 break;
             case "amarillo":
-                System.out.println("El color " + color.equalsIgnoreCase("Amarillo") + ", en inglés se escribe yellow");
+                System.out.println("El color en inglés se escribe yellow");
                 break;
             case "verde":
-                System.out.println("El color " + color.equalsIgnoreCase("Verde") + ", en inglés se escribe green");
+                System.out.println("El color en inglés se escribe green");
                 break;
             default:
                 System.out.println("No conozco ese color");
---
+++ src/daw01/examen02/Ej2.java
@@ -0,0 +1,23 @@
+package daw01.examen02;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        String dias[] = {"Lunes", "Miercoles", "Viernes"};
+        String m[] = {"M1", "M2", "M3", "M4"};
+
+        //No puedes poner un Fori mayor dentro de uno mas pequeño
+
+        int cont = 0; //lo ponemos para que vaya sumando el numero de mascarillas
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < dias.length; j++) {
+                System.out.print(dias[j] + "-" + m[cont] + " ");
+                cont++;//sumamos ++, para que al entrar en el if siga por donde se a quedado
+
+                if (cont == m.length){
+                    cont = 0;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
