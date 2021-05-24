3fff1ff2c127bb293715ade6780a498085c700f3
cmirand0 <cmirand0@jaumebalmes.net>
2021-05-24 14:07:03 +0200

    add

--- src/daw01/examenes/examenUF1a/I.java
+++ src/daw01/examenes/examenUF1a/I.java
@@ -5,31 +5,32 @@ import java.util.Scanner;
 public class I {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Número de filas en vertical (minimo 5, contando primera y ultima): ");
-        int vert = sc.nextInt();
-        System.out.print("Carácter a utilizar en el dubujo para representa la letra I: ");
-        String carct = sc.next();
 
-        int cont = 0;
-        while (vert < 5) {
-            System.out.print("Valor demasiado bajo, el valor minimo es 5");
-            vert = sc.nextInt();
-            System.out.print("Carácter a utilizar en el dubujo para representa la letra I:");
-            carct = sc.next();
-        }
+        int vertical = -1;
+        do {
+            System.out.print("Número de filas en verticalical (minimo 5, contando primera y ultima): ");
+            vertical = sc.nextInt();
+        } while (vertical < 5);
+        String caracter = "foo";
+
+        do {
+            System.out.print("Carácter a utilizar en el dubujo para representa la letra I: ");
+            caracter = sc.next();
+        } while (caracter.length() > 1);
+
         for (int i = 0; i < 3; i++) {
-            System.out.print(carct);
+            System.out.print(caracter);
         }
         System.out.println();
 
-        for (int i = 0; i < vert - 2; i++) {
-            System.out.println(" " + carct);
-            cont++;
+        for (int i = 0; i < vertical - 2; i++) {
+            System.out.println(" " + caracter);
         }
 
         for (int i = 0; i < 3; i++) {
-            System.out.print(carct);
+            System.out.print(caracter);
         }
+
         System.out.println();
     }
 }
