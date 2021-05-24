56171a9df229f928322ac23dfc9b2fbb615d89fe
cmirand0 <cmirand0@jaumebalmes.net>
2021-05-24 17:18:45 +0200

    recursividad exam uf2

--- src/daw01/examenes/examenUf2a/Ex1.java
+++ src/daw01/examenes/examenUf2a/Ex1.java
@@ -1,19 +1,49 @@
 package daw01.examenes.examenUf2a;
 
-import java.util.Enumeration;
+import java.util.ArrayList;
+
+import daw01.examenes.examenUF1a.I;
 
 public class Ex1 {
-    static int sumaCifras (int x){
-        x = ' ';
-        String numCadena = Integer.toString(x);
 
-        for (int i = 0; i < numCadena.length(); i++) {
-            numCadena.charAt(i++);
+    // private static int sumaCifras(int x) {
+    // String numCadena = Integer.toString(x);
+    // int total = 0;
+    // for (int i = 0; i < numCadena.length(); i++) {
+    // total += Integer.parseInt(numCadena.charAt(i) + "");
+    // }
+    // return total;
+    // }
+
+    private static int sumaCifras(int x) {
+        if (x < 10) {
+            return x;
+        } else {
+            return x % 10 + sumaCifras(x / 10);
         }
+        // return x < 10 ? x : x % 10 + sumaCifrasRecursidivad(x / 10);
+    }
+
+    private static  ArrayList<Integer> sumanIgualCifras(int[] v, int x) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        for (int i = 0; i < v.length; i++) {
+            if (sumaCifras(v[i]) == x) {
+                // System.out.println(v[i]);
+                numeros.add(v[i]);
+            }
+        }
+        int[] num = new int[numeros.size()];
+        // num = numeros.toArray(num);
+
+        // System.out.println(numeros);
+
+        return numeros;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumaCifras(6546852));
+        // System.out.println(sumaCifras(12345));
+        int[] array = { 9, 10, -64, 245, 1054, 73, -10225, 54, 322, 19, 475, -23, -37 };
+        System.out.println(sumanIgualCifras(array, 10));
 
     }
 }
