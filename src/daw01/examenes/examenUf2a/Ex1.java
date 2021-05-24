c842a1698ea9bb84ddccb8fa042d254663b75d20
emirand <emiran20@jaumebalmes.net>
2021-05-24 14:33:12 +0100

    uf2

---
+++ src/daw01/examenes/examenUf2a/Ex1.java
@@ -0,0 +1,20 @@
+package daw01.examenes.examenUf2a;
+
+public class Ex1 {
+//    static int sumaCifras (int x){
+//
+//        for (int i = 0; i < x; i++) {
+//
+//        }
+//        return 'a';
+//    }
+
+    public static void main(String[] args) {
+        //System.out.println(sumaCifras(6546852));
+        String numCadena = "1";
+        int numEntero = Integer.parseInt(numCadena);
+
+        if (numCadena.equals("uno"))
+        System.out.println(numEntero);
+    }
+}
