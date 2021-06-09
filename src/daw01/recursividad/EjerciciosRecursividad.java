0fca4caa32f2a7cc2d08066d15b75f501a4741ab
emirand <emirand20@jaumebalmes.net>
2021-06-09 13:15:57 +0100

    recursividad

---
+++ src/daw01/recursividad/EjerciciosRecursividad.java
@@ -0,0 +1,30 @@
+package daw01.recursividad;
+
+public class EjerciciosRecursividad {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        //mostrarArrayRecursivo(array, 0);
+        int buscar = 5;
+        int elementoEncontrado = posicionElementoRecursivo(array, buscar, 0);
+        System.out.println(elementoEncontrado);
+    }
+
+    public static void mostrarArrayRecursivo(int[] array, int indice) {
+
+        if (indice != array.length) {
+            System.out.println(array[indice]);
+            mostrarArrayRecursivo(array, indice + 1);
+        }
+    }
+
+    public static int posicionElementoRecursivo(int[] array, int buscar, int indice) {
+
+        if (indice == array.length) {
+            return -1;
+        } else if (array[indice] == buscar)
+            return indice;
+        else {
+            return posicionElementoRecursivo(array, buscar, indice + 1);
+        }
+    }
+}
---
+++ src/daw01/recursividad/FibonnacciRecorMatriz.java
@@ -0,0 +1,20 @@
+package daw01.recursividad;
+
+public class FibonnacciRecorMatriz {
+    public static void main(String[] args) {
+        int[][] m = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+    }
+
+    public static void recorrerMatrizRecursivo(int[][] m, int i, int j) {
+        System.out.print(m[i][j] + " ");
+
+        if (i != m.length || j!=m[i].length){
+
+        }
+
+    }
+}
