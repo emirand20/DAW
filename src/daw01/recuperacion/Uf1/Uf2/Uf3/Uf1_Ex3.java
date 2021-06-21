2cce4bd48d12b624803fa0502864d69824149b4a
emirand <emirand20@jaumebalmes.net>
2021-06-21 19:23:52 +0100

    recursividad

--- src/daw01/recuperacion/Uf1/Uf2/Uf3/Uf1_Ex3.java
+++ src/daw01/recuperacion/Uf1/Uf2/Uf3/Uf1_Ex3.java
@@ -4,13 +4,19 @@ public class Uf1_Ex3 {
     public static void main(String[] args) {
         String consonantes[] = {"c", "m", "s", "t", "r"};
         String vocales[] = {"a", "e", "i", "o", "u"};
-
-        for (int i = 0; i < 2; i++) {
-            int a = (int) (Math.random() * consonantes.length);
-            int b = (int) (Math.random() * vocales.length);
-            System.out.print(consonantes[a] + vocales[b] );
-
-
+        for (int x = 0; x < 5 ; x++) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 2; j++) {
+                    int a = (int) (Math.random() * consonantes.length);
+                    int b = (int) (Math.random() * vocales.length);
+                    System.out.print(consonantes[a] + vocales[b] );
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
         }
+
+
+
     }
 }
