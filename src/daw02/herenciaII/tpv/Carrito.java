56ca8f2ab6ace43fce48931818929206f7172e50
emirand20 <emirand20@jaumebalmes.net>
2021-10-19 20:02:07 +0200

    Act02

---
+++ src/daw02/herenciaII/tpv/Carrito.java
@@ -0,0 +1,9 @@
+package herenciaII.tpv;
+
+import herenciaII.tpv.Leche;
+import herenciaII.tpv.Agua;
+
+public class Carrito {
+    //Leche leche = new Leche("Pascual", 3, "Pascual", 1-2-3, 3,4, true);
+    
+}
---
+++ src/daw02/trercerAc/Shape.java
@@ -0,0 +1,44 @@
+package trercerAc;
+
+import java.awt.Point;
+import java.awt.Color;
+
+public class Shape{
+   private final Color color;
+   private final Point origin;
+   private Color col;
+   private Point org; 
+
+
+   public double area(){
+    return 0;
+   }
+   public Color getColor(){
+       return color;
+   }
+   public Point getOrigen(){
+       return origin;
+   }
+   public double perimetre(){
+       return 0;
+   }
+    public void setColor(Color color) {
+    this.col = col;
+    }
+    public void setOrigen(Point origen) {
+    this.org = org;
+    }
+    
+    protected Shape(Color col, Point org){
+        this.color = col;
+        this.origin = org;
+    }
+    @Override
+    public String toString() {
+        return "Shape [col=" + col + ", color=" + color + ", org=" + org + ", origin=" + origin + "]";
+    }
+
+    protected void Shape(){
+        this.org = org;
+    }
+}
