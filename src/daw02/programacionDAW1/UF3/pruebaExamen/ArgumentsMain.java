4c81699413b52c4c69bff372419ea8165fb6b30c
Asus <emirand20>
2022-06-07 09:51:02 +0200

    pruebaExamen

---
+++ src/daw02/programacionDAW1/UF3/pruebaExamen/ArgumentsMain.java
@@ -0,0 +1,11 @@
+package programacionDAW1.UF3.pruebaExamen;
+
+public class ArgumentsMain {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            i = (int)(Math.random() * 100);
+
+
+        }
+    }
+}
