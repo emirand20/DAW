fd6b5e1c2cae0fb8c098691adcf1a6d16882f435
emirand20 <emirand20@jaumebalmes.net>
2022-09-20 10:58:42 +0200

    M06

--- src/daw03/M06/Introduccio/practica1.js
+++ src/daw03/M06/Introduccio/practica1.js
@@ -1,6 +1,4 @@
-var inicio = 0; //se inicializa una variable en 0
-
-function aumentar(){ // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
-
-var cantidad = document.getElementById('cantidad').value = ++inicio; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
+var inicio = 0
+function aumentar(){
+var cantidad = document.getElementById('cantidad').value = ++inicio; 
 }
