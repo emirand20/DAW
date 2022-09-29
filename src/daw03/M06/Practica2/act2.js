a29341a2e58cd34d0f4471bfbc54171d9557d39c
emirand20 <emirand20@jaumebalmes.net>
2022-09-29 15:08:26 +0200

    act2

--- src/daw03/M06/Practica2/act2.js
+++ src/daw03/M06/Practica2/act2.js
@@ -1,4 +1,13 @@
-function color(){
+function color() {
     let elemento = document.getElementById("input").value;
     document.getElementById("cuadrado").style.backgroundColor = elemento;
+}
+
+function seleccion() {
+    let parentList = document.getElementById('llista');
+    let listChildren = parentList.children;
+    let elemento = document.getElementById("input").value;
+    for (let i = 0; i < listChildren.length; i++) {
+        console.log(listChildren[i]);
+    }
 }
