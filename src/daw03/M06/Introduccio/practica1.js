4d62fcff038c6db2b2e4b1782a3491e3670ae867
emirand <you@jaumebalmes.net>
2022-11-23 09:46:44 +0100

    modifPractica1Js

--- src/daw03/M06/Introduccio/practica1.js
+++ src/daw03/M06/Introduccio/practica1.js
@@ -1,19 +1,19 @@
-var inicio = 0;
+let inicio = 0;
 function aumentar() { // se crean la funcion y se agrega al evento onclick en en la etiqueta button con id aumentar
-    var cantidad = document.getElementById('cantidad').value = ++inicio; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
+    let cantidad = document.getElementById('cantidad').value = ++inicio; //se obtiene el valor del input, y se incrementa en 1 el valor que tenga.
 }
 
 function factorial(n) {
-    var total = 1;
+    let total = 1;
     for (i = 1; i <= n; i++) {
-        total = total * i;
+        total *= i;
     }
     return total;
 }
 function boto() {
-    var n = document.getElementById('fact').valueAsNumber;
+    let n = document.getElementById('fact').valueAsNumber;
     let total = factorial(n);
-    document.getElementById("num").innerHTML = total; //innerHTLM mostrara cadenas en cambio value el numero.
+    document.getElementById("num").innerHTML = total; //en el id num mostraremos la variable total ya creada
 }
 
 function letraDNI() {
@@ -23,9 +23,9 @@ function letraDNI() {
     document.formulario.dni.value = formulario.dni.value + " - " + letra
 }
 /*function letraDNI(dni) {
-    var cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
-    var posicion = dni % 23;
-    var letra = cadena.substring(posicion, posicion + 1);
+    let cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
+    let posicion = dni % 23;
+    let letra = cadena.substring(posicion, posicion + 1);
     return dni + " - " + letra;
 }*/
 //document.write(letraDNI("35738881"));
