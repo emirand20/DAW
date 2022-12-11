d75354297c4b1e2167e67e2af23f7cc73e95ed21
Asus <emirand20>
2022-12-11 16:31:32 +0100

    app.vue

---
+++ src/daw03/Vue/Tutorial/app.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hola mundo'
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
