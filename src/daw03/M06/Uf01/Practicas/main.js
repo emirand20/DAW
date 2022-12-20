3915283a60edf32928199d2152959b120dab0b00
emirand <you@jaumebalmes.net>
2022-12-20 12:23:26 +0100

    ToDo.js

---
+++ src/daw03/M06/Uf01/Practicas/main.js
@@ -0,0 +1,58 @@
+const lista = document.getElementsByTagName("div")
+//crearemos un div que tenga un input checkbox y fuera del imput
+function añadirInput() {
+    let div = document.createElement("div")
+    div.id = "TareaPorHacer"
+
+    let checkbox = document.createElement("input")
+    checkbox.id = "myCheck" 
+    checkbox.setAttribute("type", "checkbox")
+
+    let añadirTarea = document.getElementById("input").value //conectamos el input 
+    let nuevaTarea = document.createTextNode(añadirTarea)
+    
+    let span = document.createElement("span")
+    span.appendChild(nuevaTarea)
+
+    let button = document.createElement("button")
+    let txt = document.createTextNode("x")
+    button.appendChild(txt)
+
+    if (añadirTarea === '') {
+        alert("Añade una tarea");
+    } else {
+        div.appendChild(checkbox)
+        document.getElementById("lista").appendChild(div).appendChild(span).appendChild(button)
+    }
+    document.getElementById("input").value = ''
+}
+
+function updateItem() {
+    for (let i = 0; i < lista.length; i++) {
+        let checkbox = document.getElementById("myCheck")
+
+        checkbox.addEventListener('click', function () {
+            if (checkbox.checked = true) {
+                lista[i] = document.getElementById("text").style.textDecoration = "line-through"
+            }
+        })
+    }
+}
+
+function editList() {
+    document.getElementById("foo").disabled = true
+
+    // for (let i = 0; i < lista.length; i++) {
+    //     let button = document.createElement("button")
+    //     let txt = document.createTextNode("x")
+    //     button.appendChild(txt)
+    //     lista[i].appendChild(button)
+    //     button.addEventListener('click', function () { //funcion anonima
+    //         lista[i].style.display = "none"
+    //     })
+    // }
+}
+
+function verTodosElementosLI() {
+    //ha de retornar un la lista de los elementos originsles y añadidos
+}
