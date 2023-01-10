25250869d5ff953ea60ec59af7144007215d59b8
emirand <you@jaumebalmes.net>
2023-01-10 13:01:16 +0100

    ToDo.js

--- src/daw03/M06/Uf01/Practicas/ToDo/main.js
+++ src/daw03/M06/Uf01/Practicas/ToDo/main.js
@@ -1,59 +1,42 @@
-const lista = document.getElementsByTagName("div")
+const { doc } = require("prettier")
+
+const lista = document.getElementsByTagName('div')
+
 //crearemos un div que tenga un input checkbox y fuera del imput
 function añadirInput() {
-    let div = document.createElement("div")
+    let div = document.createElement('div')
 
-    let checkbox = document.createElement("input")
-    checkbox.setAttribute("type", "checkbox")
+    let checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
 
-    checkbox.addEventListener('change', function (){
+    checkbox.addEventListener('change', function (event) {
         let parent = checkbox.parentElement
         let child = parent.lastChild
-        child.style.textDecoration = "line-through"
-        
-        child.style.textDecoration = "none"
+        let child1 = parent.lastChild
+
+        if (event.currentTarget.checked) {
+            child.style.textDecoration = 'line-through'
+            // let supr = document.createElement('input')
+            // supr.setAttribute('type', 'checkbox')
+        } else {
+            child.style.textDecoration = 'none'
+        }
     })
 
-    let añadirTarea = document.getElementById("input").value //conectamos el input 
+    let añadirTarea = document.getElementById('input').value //conectamos el input 
     let nuevaTarea = document.createTextNode(añadirTarea)
-    
-    let span = document.createElement("span")
+
+    let span = document.createElement('span')
     span.appendChild(nuevaTarea)
 
-    /*let button = document.createElement("button")
-    let txt = document.createTextNode("x")
-    button.appendChild(txt)
-
-    button.addEventListener('click', function () { //funcion anonima
-        div.style.display = "none"
-    })*/
-
     if (añadirTarea === '') {
-        alert("Añade una tarea");
+        alert('Añade una tarea');
     } else {
         div.appendChild(checkbox)
-        document.getElementById("lista").appendChild(div).appendChild(span)
+        document.getElementById('lista').appendChild(div).appendChild(span)
     }
-    document.getElementById("input").value = ''
+    document.getElementById('input').value = ''
 }
-
-// function editList() {
-//     document.getElementById("foo").disabled = true
-//     //
-// }
-function updateItem() {
-    //ERROR MUESTRA TACHA EL PRIMER ID
-    for (let i = 0; i < lista.length; i++) {
-        let checkbox = document.getElementById("myCheck")
-
-        checkbox.addEventListener('click', function () {
-            if (checkbox.checked = true) {
-                lista[i] = document.getElementById("text").style.textDecoration = "line-through"
-            }
-        })
-    }
-}
-
 
 function verTodosElementosLI() {
     //ha de retornar un la lista de los elementos originsles y añadidos
