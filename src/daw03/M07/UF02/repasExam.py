eec495d73b99b4360a1be2460dc181f44ddbb4b1
emirand20 <you@jaumebalmes.net>
2023-01-10 18:52:53 +0100

    repasExam.py

--- src/daw03/M07/UF02/repasExam.py
+++ src/daw03/M07/UF02/repasExam.py
@@ -12,11 +12,12 @@ e = np.zeros((2,3))
 f = np.ones((3,4))
 # Definiendo el tipo de dato
 g = np.array(range(12), dtype='int32')
-print(g.dtype)
+#print(g.dtype)
 
 # No confundir ndarray de Numpy amb l'array de Python
 # => ndarray => numpy.array
 # => python => array.array
+
 # ---------------------------------------------------
 
 # NDARRAY metodos
@@ -28,3 +29,18 @@ print(g.dtype)
 # ndarray.data ⇒ Búfer que conté els elements reals de la matriu.
 # ndarray.tolist ⇒ Convertir una ndarray a una list.
 
+h = np.arange(15).reshape(3,5)
+print(h.shape) 
+#(3,5)
+print(h.ndim) 
+#2
+print(h.dtype)
+print(h.itemsize)
+print(h.size)
+print(type(h))
+print(h.data)
+
+# NDARRAY vs List
+i = np.array ([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+np.reshape(i(3,4))
+print(i)
