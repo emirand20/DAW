573103ff34fd44324e906b5542e745497229ed1c
Asus <emirand20>
2023-01-16 18:23:47 +0100

    py

--- src/daw03/M07/UF02/Python Pandas + MATPLOTLIB/DataFrame.py
+++ src/daw03/M07/UF02/Python Pandas + MATPLOTLIB/DataFrame.py
@@ -5,14 +5,14 @@ estadisticas = {
     "season": [2020, 2020, 2020],
     "points": [39, 28, 14]
 }
-# teams_stats = pd.DataFrame(estadisticas)  
-# print(teams_stats)
+teams_stats = pd.DataFrame(estadisticas)
+print(teams_stats)
 
 # Costruccion diccionario fila por fila
-estadistiques2 = {
-    {"teams": "Real Mallorca", "year": 2020, "points": 39},
-    {"teams": "Real Oviedo", "year": 2020, "points": 28},
-    {"teams": "Roger FC", "year": 2020, "points": 14},
-}
+estadistiques2 = [
+        {"teams": "Real Mallorca", "year": 2020, "points": 39},
+        {"teams": "Real Oviedo", "year": 2020, "points": 28},
+        {"teams": "Roger FC", "year": 2020, "points": 14},
+]
 teams_stats2 = pd.DataFrame(estadistiques2)
 print(teams_stats2)
