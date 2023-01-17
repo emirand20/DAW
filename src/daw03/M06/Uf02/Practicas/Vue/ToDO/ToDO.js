09022ec1a7bc1752e6c679378412a09c5870a9c8
emirand <you@jaumebalmes.net>
2023-01-17 13:16:15 +0100

    ToDO.vue

--- src/daw03/M06/Uf02/Practicas/Vue/ToDO/ToDO.js
+++ src/daw03/M06/Uf02/Practicas/Vue/ToDO/ToDO.js
@@ -1,7 +1,21 @@
 let app = new Vue({
-    el: '#app',
+    el: '#app', //esto es la conceccion con el ids  
     data: {
         header: 'To Do'
-    }
+    },
 })
 
+let añadirInput = new Vue({
+    data: {
+        añadirInput: {
+            click: () => ("Has hecho click")
+
+        }
+    }
+})
+var app2 = new Vue({
+    el: '#aplicacion',
+    data:{ 
+      contador: 100
+    }      
+  })
