b52a0df22ba3f10772086f017681deae1af163bf
emirand <you@jaumebalmes.net>
2023-01-23 13:13:43 +0100

    vue

--- src/daw03/M06/Uf02/Practicas/Vue/ToDO/ToDO.js
+++ src/daw03/M06/Uf02/Practicas/Vue/ToDO/ToDO.js
@@ -1,28 +1,26 @@
 let list = new Vue({
-    el: '#list', //conecta con el id del html
+    el: '#app', //conecta con el id del html
     data: {
         title: 'To Do',
         message: 'Crea una lista de tareas',
-        li: [],
-        checked: [],
-        items: []
+        nueva: null,
+        items: [
+            {
+                text: 'Tomates',   
+                checkbox: false,
+                deleted: false
+            }
+        ]
     },
     methods: {
-        addItem: function () {
-            let input = document.getElementById("itemForm");
-
-            if (input.value !== "") {
-                this.items.push({
-                    text: input.value
-                });
-                input.value = "";
-            }
+        addItem() { 
+            this.items.push({
+                text: this.nueva,
+                checkbox: false,
+                deleted: false
+            })
         },
-        deleteItem: function (index1) {
-            // let input = document.getElementsByTagName("li");
-            // const clase = input.classList.add('eliminaTarea')
-            // input.addEventListener('click', clase)
-
+        moverPapelera(index) {
             this.items.splice(index, 1); //agrega y/o elimina elementos de la matriz.
         }
     }
