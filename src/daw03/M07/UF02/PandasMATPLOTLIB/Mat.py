dfc9c93b30c7edbbe5e0a82673b50c9fabbadccc
Asus <emirand20>
2023-01-24 20:35:56 +0100

    python

---
+++ src/daw03/M07/UF02/PandasMATPLOTLIB/Mat.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+"""squares = [1,4,5,6,25]
+plt.plot(squares, 'k--')
+plt.show()"""
+fig1 = plt.figure(num='Grafica 1')
+fig2 = plt.figure(num='Grafica 2')
+plt.show()
+"""days = np.array(['Lun', 'Martes', 'Miercoles','Jueves', 'Viernes', 'Sabado', 'Domingo'])
+deaths = np.array([230, 600, 0,0,500,0,0])
+
+plt.plot(days, deaths)
+plt.xlabel('days')
+plt.ylabel('deaths')
+plt.show()"""
--- src/daw03/M07/UF02/PandasMATPLOTLIB/readCsv.py
+++ src/daw03/M07/UF02/PandasMATPLOTLIB/readCsv.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('data/population.csv', usecols=['City', 'Density KM2'])
+print(df.tail())
+#Ordenar y sumar
+#orderSum = df[(df['Population'] == 'Senglea')]
+#print(orderSum['Population'].sum())
 
-print(df.head())
+persons = {'name': ['Roger', 'Oriol', 'Maria'],'age':[18, 34,36]}
+df = pd.DataFrame(persons)
+print('----------------------')
+print(df)
+persons02 = pd.DataFrame(persons, index= ['User1', 'User2', 'User3'])
+print('----------------------')
+print(persons02)
+persons03 = pd.DataFrame(persons02, columns=['names', 'ages'])
+print('----------------------')
+print(persons03)
+#Selecionar una fila
+print('----------------------')
+print(persons02.loc[['User2'], ['age']])
