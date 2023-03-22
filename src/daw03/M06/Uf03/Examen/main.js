8a13afb0e28a85089c07e8c8b5f62100f4e2e099
emirand20 <you@jaumebalmes.net>
2023-03-22 20:55:09 +0100

    examUf03

--- src/daw03/M06/Uf03/Examen/main.js
+++ src/daw03/M06/Uf03/Examen/main.js
@@ -1,17 +1,14 @@
 import { canvas, context, alçadaPala, maximPalaY, paddleSpeed, velocitatPilota, palaEsquerra, palaDreta, pilota } from './pong.js'
 import { collides, loop } from './pong.js'
 
-window.addEventListener('mouseup', function (e) {
-    tmp = document.elementFromPoint(x + px, y + py);
-    up = palaDreta.x + palaDreta.alçada
-    down = palaDreta.y + palaDreta.amplada
-})
-
-window.addEventListener('mousemove', function (e) {
-    
-})
+document.addEventListener("mousemove", e => {
+    palaEsquerra.y = e.y;
+})  
+document.addEventListener("mousemove", e => {
+    palaEsquerra.y = e.y - 150;
+});       
 
 palaEsquerra.querySelector(window).scroll(function () {
     let pixel = document.querySelector(window).scrollTop
     macpro.currentTime = pixel / 1000
-})
+})   
